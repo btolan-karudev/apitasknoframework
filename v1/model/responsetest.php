67a4c11fa5f0c1dc7d2f770d45da679f44a0c4b5
Beniamin Tolan thinkCenter <beniamin.tolan@outlook.com>
2020-08-15 06:05:47 +0200

    CORS htacces to alow origin for all

--- v1/model/responsetest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('Response.php');
-
-$response = new Response();
-$response->setSuccess(false);
-$response->setHttpStatusCode(404);
-$response->addMessage('Error with value');
-$response->send();
